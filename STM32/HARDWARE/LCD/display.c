853ac3035315ef203daf474e341cd1f75c5770e0
hvweihong <hvweihong@foxmail.com>
2017-04-08 19:37:56 +0800

    sort out stm32

--- STM32/HARDWARE/LCD/display.c
+++ STM32/HARDWARE/LCD/display.c
@@ -8,8 +8,7 @@ void Load_Drow_Dialog(void)
 	LCD_ShowString(lcddev.width-24,0,200,16,16,"RST");//显示清屏区域
   POINT_COLOR=RED;//设置画笔蓝色 
 }
-////////////////////////////////////////////////////////////////////////////////
-//电容触摸屏专有部分
+
 //画水平线
 //x0,y0:坐标
 //len:线长度
@@ -189,3 +188,41 @@ void ctp_test(void)
 		if(i%20==0)LED0=!LED0;
 	}	
 }
+
+void init_display(void)
+{
+	int loop_index = 0;
+	POINT_COLOR=RED;//设置字体为红色 
+	LCD_ShowString(60,50,200,16,16,"project:car info display");	
+	LCD_ShowString(60,70,200,16,16,"author: weidi.wang");	
+	LCD_ShowString(60,90,200,16,16,"date:   2017/4/4");
+	
+	//画表盘
+	for(loop_index=0; loop_index < 4; loop_index++)
+	{
+		LCD_Draw_Circle(lcddev.width/2, lcddev.height*5/6, (lcddev.height/3 + loop_index));
+	}
+	for(loop_index=0; loop_index <= 180; loop_index+=30)
+	{
+		if(loop_index > 90)
+		{
+			LCD_ShowNum(380 - 180*cos(loop_index*PI/180.0), 400 - 180*sin(loop_index*PI/180.0), loop_index + 30, 3, 16);
+		}else
+		{
+			LCD_ShowNum(400 - 180*cos(loop_index*PI/180.0), 400 - 180*sin(loop_index*PI/180.0), loop_index + 30, 3, 16);
+		}
+	}
+	
+	//画刻度
+	for(loop_index=0; loop_index < lcddev.width; loop_index += 50)
+	{
+		LCD_DrawLine(loop_index, (lcddev.height - 5), loop_index, lcddev.height);	
+	}
+	
+	//初始化指针在0位置
+	show_speed(0, 100, BLUE);
+	//初始化电压 温度 显示
+	show_bar(100, 450, 50, 80, GREEN , "V");
+	show_bar(700, 450, 50, 60, GREEN , "C");
+}
+
--- STM32/HARDWARE/LCD/display.h
+++ STM32/HARDWARE/LCD/display.h
@@ -8,7 +8,9 @@
 #include "led.h"
 #include "delay.h"
 #include "math.h"
+
 #define PI 3.14159
+
 void Load_Drow_Dialog(void);
 void gui_draw_hline(u16 x0,u16 y0,u16 len,u16 color);
 void gui_fill_circle(u16 x0,u16 y0,u16 r,u16 color);
@@ -18,5 +20,6 @@ void show_bar(u16 x0, u16 y0, u8 size, u16 value, u16 color, char *descri);
 void show_speed(u8 speed, u16 length, u16 color);
 void rtp_test(void);
 void ctp_test(void);
+void init_display(void);
 
 #endif
